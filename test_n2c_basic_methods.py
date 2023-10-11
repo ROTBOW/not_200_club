ec78401432de392f2b1792119df21b82b3885f77
Josiah Leon <sjosiahleon@gmail.com>
2023-10-11 12:35:19 -0700

    removed test from deprecated method

--- test_n2c_basic_methods.py
+++ test_n2c_basic_methods.py
@@ -8,14 +8,6 @@ def test_can_instantiate_class():
     n2c = Not200Club(None)
     assert isinstance(n2c, Not200Club)
 
-def test_idx_to_letter():
-    n2c = Not200Club(None)
-    assert n2c._Not200Club__idx_to_letter(0) == 'A'
-    assert n2c._Not200Club__idx_to_letter(25) == 'Z'
-    assert n2c._Not200Club__idx_to_letter(26) == 'AA'
-    assert n2c._Not200Club__idx_to_letter(27) == 'AB'
-    assert n2c._Not200Club__idx_to_letter(28) == 'AC'
-
 def test_validate_url():
     n2c = Not200Club(None)
     assert n2c._Not200Club__validate_url('https://www.google.com') == 'https://www.google.com'
