0c9917c541c8a33d16ebce6cc3fae8356d80e085
Josiah Leon <sjosiahleon@gmail.com>
2023-12-15 23:13:37 -0800

    added inline comments for future understanding

--- emailer.py
+++ emailer.py
@@ -24,15 +24,15 @@ class Emailer:
         The function finds the most recent file in res folder and sets the
         `file_path` attribute to the path of that file.
         """
-        files = os.listdir(RES)
+        files = os.listdir(RES) # get the files from the RES folder
         most_recent_file = [0, '']
         for f in files:
             t = os.path.getmtime(os.path.join(RES, f))
-            if t > most_recent_file[0]:
+            if t > most_recent_file[0]: # keep track of the most recent file
                 most_recent_file = [t, f]
                 
-        self.file_path = os.path.join(RES, most_recent_file[1])
-        self.__validate_file()
+        self.file_path = os.path.join(RES, most_recent_file[1]) # when we find it, save it's path to as one of our instance variables
+        self.__validate_file() # check that the file we just grab was created today
         
     
     
@@ -53,9 +53,10 @@ class Emailer:
         The function validates if the file's creation date matches today's date and prompts the user for
         confirmation before proceeding with the emailer.
         """
-        d = datetime.fromtimestamp(os.path.getctime(self.file_path))
+        d = datetime.fromtimestamp(os.path.getctime(self.file_path)) # get the datetime for the target file and today
         today = datetime.now()
-        if not all([today.day == d.day, today.month == d.month, today.year == d.year]):
+        
+        if not all([today.day == d.day, today.month == d.month, today.year == d.year]): # unless the file was created today it asks the script runner if they are sure they want to run it.
             print('File\'s date does not match today\'s date!')
             print(f'    - today: {today.month}-{today.day}-{today.year}')
             print(f'    -  file: {d.month}-{d.day}-{d.year}')
@@ -76,26 +77,26 @@ class Emailer:
         The `scan_sheets` function scans through sheets in an Excel workbook, skips certain sheets, and
         sends data from other sheets via email.
         """
-        workbook = openpyxl.load_workbook(self.file_path)
-        context = ssl.create_default_context()
+        workbook = openpyxl.load_workbook(self.file_path) # opening up the xlsx file
+        context = ssl.create_default_context() # context for the emailing server
         
-        with smtplib.SMTP_SSL('smtp.gmail.com', port=465, context=context) as server:
-            server.login(os.getenv('email_user'), os.getenv('email_password'))
+        with smtplib.SMTP_SSL('smtp.gmail.com', port=465, context=context) as server: # starting up the server and logging in to our emailer account
+            server.login(os.getenv('email_user'), os.getenv('email_password')) # the login info is stored in a .env for security
             
-            with alive_bar(len(workbook.worksheets), title='Getting Coach\'s summary and emailing...') as bar:
-                for sheet in workbook.worksheets:
-                    if sheet.title in {'Overview', 'Issue Legend', 'Placements'}:
+            with alive_bar(len(workbook.worksheets)-3, title='Getting Coach\'s summary and emailing...') as bar: # setup for alive progress bc it looks nice
+                for sheet in workbook.worksheets: # going over the sheets to scrap the data and send the emails
+                    if sheet.title in {'Overview', 'Issue Legend', 'Placements'}: # we want to skip these three since they don't have a coach assigned to them
                         continue
-                    elif os.getenv(self.__format_name(sheet.title)) == None:
-                        self.no_emails.append(sheet.title)
+                    elif os.getenv(self.__format_name(sheet.title)) == None: # If we don't have an email associated with the sheet we skip it
+                        self.no_emails.append(sheet.title) # also take note of it so we can notify the script runner later
                         continue
                     
-                    bar.title(f'Getting {sheet.title.split(" ")[0]}\'s summary and emailing...')
-                    self.get_data_and_email(sheet, server)
+                    bar.title(f'Getting {sheet.title.split(" ")[0]}\'s summary and emailing...') # updating the alive progress bar with the curret sheet/coach
+                    self.get_data_and_email(sheet, server) # method for creating the email and sending it
                     
                     bar()
                 
-        if self.no_emails:
+        if self.no_emails: # if we have sheets with no email, we listed them after
             print('the following names didn\'t have a email listed')
             print(self.no_emails)
                 
@@ -120,18 +121,19 @@ class Emailer:
         
         for row in sheet.iter_rows():
             name, status, solo, capstone, group = (x.value for x in row)
-            if status == 'Seeker Status':
+            if status == 'Seeker Status': # this skips the first row of the sheet
                 continue
             
-            if 'No Issues Found' not in (solo, capstone, group):
+            if 'No Issues Found' not in (solo, capstone, group): # if all projects have something wrong with them, put the seeker in the danger_zone
                 danger_zone.append([name, status])
                 
-            for proj in (solo, capstone, group):
+            for proj in (solo, capstone, group): # grab misc data to put in the email
                 if proj.startswith('time:'):
                     time += 1
                 if proj.split(':')[0] in ('timeout', 'status', 'bad_url', 'no-link'):
                     redzone += 1
             
+        # construct the email, a plaintext and html version
         message = MIMEMultipart("alternative")
         message["Subject"] = "Seeker Site Health Summary"
         message["From"] = sender_email
@@ -176,10 +178,11 @@ class Emailer:
         </html>
         '''
             
+        # attatching both versions to the message
         message.attach(MIMEText(text, 'plain'))
         message.attach(MIMEText(html, 'html'))
         
-        server.sendmail(sender_email, receiver_email, message.as_string())
+        server.sendmail(sender_email, receiver_email, message.as_string()) # send the email
         
         
 if __name__ == '__main__':
